de38fd5a48d9c379b4b8ec576e599950319bcf96
LARA088 <128611780+LARA088@users.noreply.github.com>
2024-04-22 08:42:41 -0300

    ok

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for(let i=0; i<botoes.length;i++){
 }
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-13T23:59:59");
-const tempoObjetivo2 = new Date("2024-11-30T23:59:59");
-const tempoObjetivo3 = new Date("2024-09-01T23:59:59");
-const tempoObjetivo4 = new Date("2024-07-30T23:59:59");
+const tempoObjetivo2 = new Date("2024-12-31T23:59:59");
+const tempoObjetivo3 = new Date("2024-11-05T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-13T23:59:59");
 
 const tempos=[tempoObjetivo1, tempoObjetivo2, tempoObjetivo3,tempoObjetivo4];
 for (let i=0; i < contadores.length; i++){
